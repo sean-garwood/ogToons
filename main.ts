05a5fad5c6aea7acef3ea83eae78724db7453e81
Joe Naegele <joseph.naegele@gmail.com>
2025-02-22 18:37:40 -0500

    chore: get colors

--- main.ts
+++ main.ts
@@ -1,4 +1,22 @@
 import * as cards from './cards.json';
+class Student {
+  fullName: string;
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
+class Card {
+  constructor(
+    public name: string,
+    public color: string, // TODO: Enum
+  ) {
+  }
+}
 
 console.log(cards[0].name);
 
@@ -21,7 +39,7 @@ const deck2 = generateRandomDeck();
 
 // Start the game!
 
-function pluck(deck: any[]) {
+function pluck(deck: Card[]) {
     const randomIndex = Math.floor(Math.random() * deck.length);
     return deck[randomIndex];
 }
@@ -35,3 +53,28 @@ const bottomCard2 = deck2[deck2.length - 1];
 
 console.log('Player 1 reveals', bottomCard1.name);
 console.log('Player 2 reveals', bottomCard2.name);
+
+// we need to determine the colors of the game
+
+// colors is an array that has zero to two members
+
+// REMEMBER: SILVER and BLACK are null colors; they can't be used to determine the color of the game
+
+// check first card
+// check second card
+
+
+
+function getColors(deck1: Card[], deck2: Card[]) {
+    const color1 = deck1[deck1.length - 1].color;
+    const color2 = deck2[deck2.length - 1].color;
+
+    console.log('Color 1:', color1);
+    console.log('Color 2:', color2);
+
+}
+
+// TODO: color cut logic
+
+const colors = getColors(deck1, deck2);
+console.log('Colors:', colors);
